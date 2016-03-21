ea65ab08d078022c79544fa7923e10e5b732bcc2
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-20 22:06:37 -0700

    added get routes as well as delete. working on PUT bug

--- cityRouter.js
+++ cityRouter.js
@@ -2,7 +2,7 @@
 let express = require('express');
 // let app = express();
 let CityRouter = express.Router();
-// let bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 let AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
 let s3 = new AWS.S3();
@@ -10,20 +10,37 @@ let s3 = new AWS.S3();
 require('./server');
 
 
-// CityRouter.use(bodyParser.json());
+CityRouter.use(bodyParser.json());
 
 CityRouter.get('/cities', (req, res)=>{
   console.log('here is get request on /cities');
+  var params = {Bucket: 'sawabucket', Key:'cities' };
+  s3.getObject(params, (err, data)=>{
+    if(err){
+      return console.log('AWS error : ' + err);
+    }
+    res.send(data.Body);
+    res.end();
+  });
 });
 
-CityRouter.get('/city/:id', (req, res)=>{
-  console.log('here is get/:id request on /city/:id');
+CityRouter.get('/cities/:id', (req, res)=>{
+  console.log('here is get/:id request on /cities/:id');
+  var idUrl = req.params.id;
+  var params = {Bucket: 'sawabucket', Key: idUrl};
+  s3.getObject(params, (err, data)=>{
+    if(err){
+      return console.log('AWS error : ' + err);
+    }
+    res.send(data.Body);
+    res.end();
+  });
 });
 
 CityRouter.post('/cities', (req, res)=>{
   console.log('here is post request on : ' + req.url);
   s3.createBucket({Bucket: 'sawabucket'},()=>{
-    var params = {Bucket: 'sawabucket', Key: 'myKey', Body: 'my first bucket!'};
+    var params = {Bucket: 'sawabucket', Key: 'cities', Body: 'my first bucket!'};
     s3.upload(params, (err, data)=>{
       console.log(data);
       if(err){
@@ -31,6 +48,7 @@ CityRouter.post('/cities', (req, res)=>{
       } else {
         console.log('Successfully uploaded data : ' + data);
       }
+      res.send(data.Body);
       res.end();
     });
   });
@@ -38,11 +56,32 @@ CityRouter.post('/cities', (req, res)=>{
 
 
 CityRouter.put('/city/', (req, res)=>{
-  console.log('here is put request on /cities');
+  var idUrl = req.query.Key;
+  console.log(idUrl);
+  console.log(req.body);
+  console.log('here is PUT request on /cities');
+  var params = {Bucket: 'sawabucket', Key: idUrl, Body: 'This is new data'};
+  s3.putObject(params, (err, data)=>{
+    if(err){
+      return console.log('AWS error : ' + err);
+    }
+    res.send(data.Body);
+    console.log(data.Body);
+    res.end();
+  });
 });
 
 CityRouter.delete('/cities/:id', (req, res)=>{
   console.log('here is delete request on /cities');
+  var idUrl = req.params.id;
+  var params = {Bucket: 'sawabucket', Key: idUrl};
+  s3.deleteObject(params, (err, data)=>{
+    if(err){
+      return console.log('AWS error : ' + err);
+    }
+    res.send(data + ' object has been deleted!');
+    res.end();
+  });
 });
 
 module.exports = CityRouter;
